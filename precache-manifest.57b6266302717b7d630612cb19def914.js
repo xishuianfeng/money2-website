bcb634c7450de04ada2005c10fc3085d323594df
xsaf <1767406957@qq.com>
2022-09-25 17:49:30 +0800

    deploy

---
+++ precache-manifest.57b6266302717b7d630612cb19def914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d107e7f6a2cac7adf7e",
+    "url": "/money2-website/css/app.a212ac63.css"
+  },
+  {
+    "revision": "eb9dbf38671a60065827b95d0e880c26",
+    "url": "/money2-website/index.html"
+  },
+  {
+    "revision": "6d107e7f6a2cac7adf7e",
+    "url": "/money2-website/js/app.75a92626.js"
+  },
+  {
+    "revision": "3e66288e5d89c3b19ba1",
+    "url": "/money2-website/js/chunk-vendors.b9e5f141.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money2-website/robots.txt"
+  }
+]);
